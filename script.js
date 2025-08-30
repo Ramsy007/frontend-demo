a41a65d8c855f32e0fa518aa2d6f8440a70ae416
Ramsy007 <girdharmishra10102003@gmail.com>
2025-08-30 17:13:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("clickBtn").addEventListener("click", () => {
+  const message = document.getElementById("message");
+  message.innerText = "🎉 Button Clicked! This app is working perfectly.";
+});
